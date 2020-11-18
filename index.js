9c1bf86cc2d3bb803d294e1defbabd6aa0cd6578
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-11-18 17:28:37 +0900

    handleMessageEvent added

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ const lineBot = (req,res) => {
             case 'follow'://友達登録で発火
                 promises.push(greeting_follow(ev));
                 break;
+            case 'message'://メッセージが送られてきたら発火
+                promises.push(handleMessageEvent(ev));
+                break;
+            
         }
     }
     Promise
@@ -39,5 +43,16 @@ const lineBot = (req,res) => {
     return client.replyMessage(ev.replyToken,{
         "type":"text",
         "text":`${profile.displayName}さん、友達登録ありがとうございます\uDBC0\uDC04\n\nタイマッサージ店〇〇です\n\nご予約お待ちしております\uDBC0\uDC01\uDBC0\uDC2D\uDBC0\uDC2D`
+    });
+ }
+ //handleMessageEvent関数（メッセージが送られてきた時の処理振り分け）
+ const handleMessageEvent = async (ev) => {
+    const profile = await client.getProfile(ev.source.userId);
+    const text = (ev.message.type === 'text') ? ev.message.text : '';
+    
+    return client.replyMessage(ev.replyToken,{
+        "type":"text",
+            "wrap": true,
+            "text":`${profile.displayName}さん\nメッセージありがとうございます。\n\n申し訳ございませんが、このアカウントでは個別の返信をしておりません。\n\n＜お問い合わせ＞\nご質問などお問い合わせは店舗にお願いします。\nhttps://tsukumonetwork.co.jp/`
     });
  }
