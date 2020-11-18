2e82f8c0ed662876cca88726ea7f871f8e95c36a
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-11-18 16:55:28 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ const client = new line.Client(config);
 app
    .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
    .listen(PORT,()=>console.log(`Listening on ${PORT}`));
+
+//lineBot関数（イベントタイプによって実行関数を振り分け）
+const lineBot = (req,res) => {
+    res.status(200).end();
+    const events = req.body.events;
+    const promises = [];
+    for(let i=0;i<events.length;i++){
+        const ev = events[i];
+        switch(ev.type){
+            case 'follow'://友達登録で発火
+                promises.push(greeting_follow(ev));
+                break;
+        }
+    }
+    Promise
+        .all(promises)
+        .then(console.log('all promises passed'))
+        .catch(e=>console.error(e.stack));
+ }
+ //greeting_follow関数(友達登録時の処理)
+ const greeting_follow = async (ev) => {
+    const profile = await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken,{
+        "type":"text",
+        "text":`${profile.displayName}さん、友達登録ありがとうございます\uDBC0\uDC04\n\nタイマッサージ店〇〇です\n\nご予約お待ちしております\uDBC0\uDC01\uDBC0\uDC2D\uDBC0\uDC2D`
+    });
+ }
