37010c1aeef3dac08cf786d8bd488022239bb72b
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2021-01-03 17:25:41 +0900

    git push heroku master

--- public/js/users.js
+++ public/js/users.js
@@ -1,19 +1,17 @@
-(()=>{ 　
-    const API_URL = 'https://sample-thai-reservation.herokuapp.com/api/';//★APIサーバのアドレス
-
+(()=>{ 
+    const API_URL = 'https://linebot-reservation.herokuapp.com/api/'; //★APIサーバアドレス
     window.addEventListener('load',()=>{ 
         fetchData(); 
     }); 
-
     const fetchData = async () => { 
-        try{ 
-            const response = await fetch(API_URL); 
+        try{ const response = await fetch(API_URL); 
             console.log('response:',response); 
             const data = await response.json(); 
-            console.log('data:',data); 
+            console.log('data:',data);
         }catch(error){ 
-            alert('データ読み込み失敗です'); 
+            alert('データ読み込み失敗です'); } 
         } 
-    }
-    
-})();
+
+
+        
+    })();
--- routers/api.js
+++ routers/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/api'); 
 
 router 
-    .route('/') 
-    .get(controller.getData); 
-    
+.route('/')
+.get(controller.getData);
+
 module.exports = router;
